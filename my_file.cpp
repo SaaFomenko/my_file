3ef57548b24bae95c1342006a6e1f19cc2523db8
SaaFomenko <s.fomenko@list.ru>
2023-05-31 20:02:17 +0300

    Need change class MyFile for read and write in single object.

--- my_file.cpp
+++ my_file.cpp
@@ -1,32 +1,142 @@
+#include <iostream>
 #include <cstring>
 #include "my_file.h"
 
 
 
-class MyException : public std::exception
+static const char* err_open_file_msg = "Error opening input file: ";
+static const char* err_write_file_msg = "Error opening for write file: ";
+
+MyException::MyException(const char* msg_) :
+    msg(msg_)
+{}
+
+MyException::~MyException()
+{}
+
+const char* MyException::what() const noexcept
 {
-    private:
-    const char* msg;
+    return msg;
+}
 
-    public:
-        MyException(const char* msg_) : msg(msg_){}
-        virtual ~MyException(){}
+MyFile::MyFile(const char* path_, bool isRw_, const unsigned int segment_) :
+    path(path_),
+    segment(segment_),
+    isRw(isRw_)
+{
+    if (isRw)
+    {
+        write();
+    }
+    else
+    {
+        read();
+    }
 
-        const char* what() const noexcept override
+    std::cout << "MyFile object build.\n";
+}
+
+MyFile::~MyFile()
+{
+    delete[] data;
+    data = nullptr;
+    fin.close();
+    fout.close();
+}
+
+void MyFile::data_resize(int i)
+{
+    if (i >= segment)
+    {
+        char* temp = new char[i + segment];
+        for (int j = 0; j >= i; ++j)
         {
-            return msg;
+            temp[j] = data[j];
         }
-};
+        delete[] data;
+        data = temp;
+    }
+}
 
-MyFile::MyFile(const char* path_) :
-    path(path_)
+void MyFile::write(const char* data_)
+{
+    fout.open(path);
+    
+    if (fout.fail())
+    {
+        throw MyException(err_write_file_msg);
+    }
+
+    data = new char[segment];
+    int i = 0;
+    while (data_[i] != 0)
+    {
+        data_resize(i);
+
+        data[i] = data_[i];
+        ++i;
+    }
+
+    size = i + 1;
+
+}
+
+void MyFile::read()
 {
     fin.open(path);
     if (fin.fail())
     {
-        char* err_open_file_msg = "Error opening input file: ";
-        strcat(err_open_file_msg, path);
         throw MyException(err_open_file_msg);
     }
-    
+    else
+    {
+        data = new char[segment]{};
+        
+        unsigned int i = 0;
+        while (!fin.eof())
+        {
+            data_resize(i);
+
+            fin >> data[i];
+            ++i;
+        }
+
+        size = i + 1;
+    }
+}
+
+const char* MyFile::to_str()
+{
+    // std::cout << "Devider: ";
+    // int i = 0;
+
+    // while (devider[i] == 0)
+    // {
+    //     std::cout << devider[i];
+    // }
+    // std::cout << "\n";
+
+    // std::cout << "Char string: ";
+    // for (int i = 0; i <= size; ++i)
+    // {
+    //     std::cout << data[i];
+    //     if (data[i] == 0)
+    //     {
+    //         return "End char string.";
+    //     }
+    // }
+    // std::cout << "\n";
+
+    unsigned int size_str = size + 1;
+    char* temp = new char[size_str];
+
+    for (int i = 0; i < size; ++i)
+    {
+        temp[i] = data[i];
+    }
+
+    temp[size] = 0;
+    data_str = temp;
+
+    return data_str;
 }
--- my_file.h
+++ my_file.h
@@ -4,20 +4,39 @@
 
 #include <fstream>
 
+class MyException : public std::exception
+{
+	private:
+	const char* msg;
+
+	public:
+		MyException(const char* msg_);
+		virtual ~MyException();
+
+		const char* what() const noexcept override;
+};
+
 class MyFile
 {
 	private:
+		const unsigned int segment;
 		const char* path;
 		std::ifstream fin;
 		std::ofstream fout;
 		char* data;
+		const char* data_str;
 		unsigned int size;
+		bool isRw;
+
+		void data_resize(int i);
+		void write(const char* data_ = "");
+		void read();
 
 	public:
-		MyFile(const char* path_);
+		MyFile(const char* path_, bool isRw_ = false, const unsigned int segment_ = 1024);
 		virtual ~MyFile();
 	
-	const char* file_to_chars();
+	const char* to_str();
 };
 
 #endif
